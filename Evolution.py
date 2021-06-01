d650f44f522fa3f1e4065b53cca594440b2312e3
GibaudNewton <gibaudsylvain@gmail.com>
2021-06-01 18:45:29 +0200

    Equation Evol Ecrite

---
+++ Evolution.py
@@ -0,0 +1,7 @@
+"""
+
+DP(W(t) = w) = 2(1-v/c)^2 P(W = w -v/2 - y/2) + 2(1-v/c)v/c[P(W = w -v - y/2) + P(W = w - y/2)] + (v/c)^2 [P(W = w - v - y/2) + P(W = w + c - y/2)] - 2 P(W = w) - Dw
+
+"""
+
+#Dw = delta P(W = w) si w \neq 0 et D0 = delta (P(W = 0) -1)
