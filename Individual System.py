28f46b0caa1acb8eeb6ab8ea6523e72621255614
Professeur Gibaud <mail@professeurgibaud.ovh>
2021-06-11 00:36:56 +0200

    GG

--- Individual System.py
+++ Individual System.py
@@ -18,8 +18,8 @@ class Individual:
     def __init__(self):
         self.w = 0
         self.name = -1
-        self.pH = v/c
-    def ProbaH(self,signal):
+        self.pH = -1
+    def ProbaH(self,signal,v,c,y,delta): #y,delta useless but easier to adapt and code
         global Wealth_Strengh
         if not Wealth_Strengh:
             self.pH = 1/2
@@ -33,22 +33,20 @@ class Individual:
                 self.pH = 1
             else: #Vérifier le else
                 self.pH = 0
-    def Depreciation(self):
-        global delta
+    def Depreciation(self,v,c,y,delta):
         #print(str(self.name) + " Depreciation")
         if rd.random()< delta:
             self.w = 0
 
 
 
-def Jeu(I1,I2):
-    global Dico
+def Jeu(I1,I2,Dico,v,c,y,delta):
     #print(str(I1.name) + " " + str(I2.name) + "Game")
     Signal1 = I1.w
     Signal2 = I2.w
     f = 1/2*(Wealth_Strengh == False) + (np.exp(lamda*Signal1)/(np.exp	(lamda*Signal1) + np.exp(lamda * Signal2)))*(Wealth_Strengh == True)
-    I1.ProbaH(Signal2)
-    I2.ProbaH(Signal1)
+    I1.ProbaH(Signal2,v,c,y,delta)
+    I2.ProbaH(Signal1,v,c,y,delta)
     A1 = 1*(rd.random() < I1.pH)  # 1 = Hawk, 0 = Dove
     A2 = 1*(rd.random() < I2.pH)
     if A1 == 0 and A2 == 0:
@@ -77,24 +75,55 @@ for i in range(N):
     I.name = i
     Dico[i] = I
 
-def Event():
-    global Dico
+def Event(Dico,v,c,y,delta):
     if rd.random()< 1/2:
-        rd.choice(Dico).Depreciation()
+        rd.choice(Dico).Depreciation(v,c,y,delta)
     else:
         A = 0
         B = 0
         while A == B:
             [A,B] = rd.choices(Dico,k=2)
-        Jeu(A,B)
-
-for i in range(T):
-    print(i)
-    Event()
+        Jeu(A,B,Dico,v,c,y,delta)
 
 
 
 
+def Simu(v,c,y,delta):
+    global N,T
+    Dico = {}
+    for i in range(N):
+        I = Individual()
+        I.name = i
+        Dico[i] = I
+    for i in range(T):
+        print(i)
+        Event(Dico,v,c,y,delta)
+    Wealth = [I.w for I in Dico.values()]
+    mean = np.mean(Wealth)
+    return mean
+
+
+y = 100
+
+ListeC = []
+v = 20
+delta = 0.1
+
+for c in range(0,80):
+    mean = Simu(v,c,y,delta)
+    ListeC.append(mean)
+
+ListeV = []
+c = 40
+
+for v in range(0,80):
+    mean= Simu(v,c,y,delta)
+    ListeV.append(v,c,y,delta)
+
+
+
+
+"""
 
 Name = [I.name for I in Dico.values()]
 Wealth = [I.w for I in Dico.values()]
@@ -112,3 +141,4 @@ with open("SaveWithStrength.txt","w+") as file:
 plt.plot(range(max(Wealth)+1),HistoWealth)
 plt.savefig("Wealth Acc Strength.jpg")
 plt.show()
+"""
